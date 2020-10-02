528792f1d2249f77890cd129e13fd59f09a91ced
torhaala <torhaala@gmail.com>
2020-10-02 06:25:09 +0200

    Add files via upload

---
+++ Load_Media.lua
@@ -0,0 +1,35 @@
+local LSM = LibStub('LibSharedMedia-3.0')
+local AddOnName = 'ShredzUI'
+local Path = [[Interface\AddOns\]]..AddOnName..[[\media\]]
+
+LSM:Register('statusbar','BuiFlat', Path..[[StatusBars\Flat.tga]])
+LSM:Register('statusbar','BuiOnePixel', Path..[[StatusBars\BuiOnePixel.tga]])
+LSM:Register('statusbar','BuiMelli', Path..[[StatusBars\Melli.tga]])
+LSM:Register('statusbar','BuiMelliDark', Path..[[StatusBars\MelliDark.tga]])
+LSM:Register('statusbar','BuiEmpty', Path..[[StatusBars\Empty.tga]])
+LSM:Register('statusbar','BuiKait1', Path..[[StatusBars\Kait1.tga]])
+LSM:Register('statusbar','BuiKait2', Path..[[StatusBars\Kait2.tga]])
+LSM:Register('statusbar','Whoa', Path..[[StatusBars\Whoa.blp]])
+LSM:Register("statusbar", "Azerite_MeterBar", Path..[[StatusBars\meter_bar.tga]]) 
+LSM:Register("statusbar", "Azerite_Nameplate_Backdrop", Path..[[StatusBars\nameplate_backdrop.tga]]) 
+LSM:Register("statusbar", "Azerite_Nameplate_Bar", Path..[[StatusBars\nameplate_bar.tga]]) 
+LSM:Register("statusbar", "Azerite_Nameplate_Glow", Path..[[StatusBars\nameplate_glow.tga]]) 
+
+LSM:Register('font','Basket of Hammers', Path..[[Fonts\Basket of Hammers.ttf]])
+LSM:Register('font','Pepsie', Path..[[Fonts\Pepsie.ttf]])
+LSM:Register('font','Skratch Punk', Path..[[Fonts\Skratch Punk.ttf]])
+LSM:Register('font','Zombie', Path..[[Fonts\Zombie.ttf]])
+LSM:Register('font','Bui Prototype', Path..[[Fonts\Prototype.ttf]])
+LSM:Register('font','Bui PrototypeRU', Path..[[Fonts\Prototype_RU.ttf]])
+LSM:Register('font','Bui Visitor1', Path..[[Fonts\Visitor1.ttf]])
+LSM:Register('font','Bui Visitor2', Path..[[Fonts\Visitor2.ttf]])
+LSM:Register('font','Bui Tukui', Path..[[Fonts\Uf_font.ttf]])
+LSM:Register('font','Bui Kait', Path..[[Fonts\HeavyDockKait.ttf]])
+LSM:Register('font','ArialN', Path..[[Fonts\ArialN.ttf]])
+LSM:Register('font','Arial', Path..[[Fonts\Arial.ttf]])
+LSM:Register('font','Bui Homizio', Path..[[Fonts\homizio_bold.ttf]])
+
+LSM:Register('sound', 'Warning', Path..[[Sounds\Warning.mp3]])
+LSM:Register('sound', 'Mail', Path..[[Sounds\mail.ogg]])
+
+LSM:Register("background", "Azerite_Meter_Case", Path..[[\Backgrounds\meter_case.tga]]) 
